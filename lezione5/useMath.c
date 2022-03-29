d76e42ef23f7c9a88a88650bf18de17148185f9e
AndreaCeccarelli <andrea.ceccarelli@unifi.it>
2022-03-29 16:19:11 +0200

    Update useMath.c

--- lezione5/useMath.c
+++ lezione5/useMath.c
@@ -3,19 +3,16 @@
 
 int main()
 {
-//alcune prove: 
-// cc -lm useMath.c -o nomeoggetto 
+//alcune prove:
+// cc -lm useMath.c -o nomeoggetto
 // cc -static -lm useMath.c -o nomeoggetto
 // cc -static useMath.c -lm -o nomeoggetto
-
-// Investigare con "nm nomeoggetto.o|grep 
-// fmod" il link a fmod:
-//   - per il file oggetto (compilare: gcc -c useMath)
-//   - per eseguibile con linking statico e dinamico
-
 //Verificare dimensione eseguibile con linking statico e dinamico
 
-	int k= fmod (3,2);
-	printf("\n %d \n", k);
-	return 0;
-} 
+// Verificare con "nm useMath.o" che gcc -c crea solo tabella dei simboli:
+
+
+        int k= fmod (3,2);
+        printf("\n %d \n", k);
+        return 0;
+}
