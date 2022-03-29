b7de535eb62ec51fe38b9a895e8bb7855d28163f
AndreaCeccarelli <andrea.ceccarelli@unifi.it>
2022-03-29 14:20:55 +0200

    Update parentesi.c

--- lezione5/parentesi.c
+++ lezione5/parentesi.c
@@ -2,11 +2,13 @@
 #define square(a) ((a)*(a))
 #define square2(a) (a*a)
 #define square3(a) a*a
-int main(void){ 
-	int j=0;
-	j = 2 * square(3+4);
-	j = 2 * square2(3+4);
-	j = 2 * square3(3+4);
+int main(void){
+        float j=0;
+        j = 2 * square(3+4);
+        printf("%f\n", j);
+        j = 2 * square2(3+4);
+        printf("%f\n", j);
+        j = 2 * square3(3+4);
+        printf("%f\n", j);
 return 0;
 }
-
