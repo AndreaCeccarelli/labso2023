6ddf3ddfb689a9c28b442570577a079dd6cbf2ae
AndreaCeccarelli <andrea.ceccarelli@unifi.it>
2023-03-16 17:02:52 +0100

    Create esercizio3lezione3.c

---
+++ soluzioni/lezione3/esercizio3lezione3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void scambia(int *,int *);
+main( )
+{
+ int x, y;
+ printf ("\n Dammi due interi decimali\n");
+ scanf("%d %d",&x,&y);
+ printf("%d%d",x,y);
+ scambia(&x,&y);
+ printf("%d%d",x,y);
+}
+
+ void scambia(int *a, int *b)
+{
+ int z;
+ z=*a;
+ *a=*b;
+ *b=z;
+ printf("%d%d",*a,*b);
+}
