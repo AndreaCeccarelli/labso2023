38cd3030caa8ccc43bc6c69a3ad6ec3785575cb1
AndreaCeccarelli <andrea.ceccarelli@unifi.it>
2023-03-16 17:01:42 +0100

    Create booleansample.c

---
+++ soluzioni/lezione3/booleansample.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+//#include <stdbool.h> //C99
+
+typedef enum {false, true} bool;
+
+int main(){
+
+	printf("false = %d \n", false);
+
+	printf("true = %d \n", true);
+
+	return 0;
+}
